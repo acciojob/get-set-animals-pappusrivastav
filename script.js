062a6457f82418e96bba650dc4ab3df9cf5a6b24
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-12-04 13:30:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Animal {}
+// class Animal {}
 
-class Dog extends Animal {}
+// class Dog extends Animal {}
 
-class Cat extends Animal {}
+// class Cat extends Animal {}
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+// Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("Some generic animal sound");
+  }
+}
+
+// Cat class (subclass of Animal)
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog class (subclass of Animal)
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
